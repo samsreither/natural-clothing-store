fb76820df290417821488cbed8a3ff9ffdf4bb90
Sam Reither <samsreither@gmail.com>
2025-02-14 21:45:09 -0500

    working on error messaging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import { userRouter } from './routes/user';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/user", userRouter);
 
 mongoose.connect(
     process.env.MONGO_URI
