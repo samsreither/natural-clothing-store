7a29f4aba847adea713f2b9071fc090e5ee3e368
Sam Reither <samsreither@gmail.com>
2025-02-14 21:10:21 -0500

    .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://samsreither:natural-clothing-store@natural-clothing-store.dnvt3.mongodb.net/"
+    process.env.MONGO_URI
 ).then(() => console.log("Connected to Mongo DB!"))
 
 app.listen(3001,() => console.log("Server Started"));
