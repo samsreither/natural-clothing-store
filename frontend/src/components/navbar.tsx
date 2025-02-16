7b5e5edcad333d72d7a4e82dba2749aac3458e62
Sam Reither <samsreither@gmail.com>
2025-02-16 13:40:41 -0500

    edits

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-title">
+        <h1> Natural Clothing Store </h1>
+      </div>
 
-
-    return <div>
-        <div className="navbar-title">
-            <h1> Natural Clothing Store </h1>
-        </div>
-
-        <div className="navbar-links">
-            <Link to="/"> Shop </Link>
-            <Link to="/purchased-items"> Purchases </Link>
-            <Link to="/checkout"> Cart </Link>
-            <Link to="/auth"> Logout </Link>
-        </div>
+      <div className="navbar-links">
+        <Link to="/"> Shop </Link>
+        <Link to="/purchased-items"> Purchases </Link>
+        <Link to="/checkout"> Cart </Link>
+        <Link to="/auth"> Logout </Link>
+      </div>
     </div>
-}
+  );
+};
