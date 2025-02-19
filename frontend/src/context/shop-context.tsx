405f45c9cb0e9fe7c56b89ebc59cbec003524832
Sam Reither <samsreither@gmail.com>
2025-02-19 10:23:52 -0500

    edits

--- frontend/src/context/shop-context.tsx
+++ frontend/src/context/shop-context.tsx
@@ -1,4 +1,10 @@
 import { createContext, useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { useGetProducts } from "../hooks/useGetProducts";
+import { useGetToken } from "../hooks/useGetToken";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { IProduct } from "../models/interfaces";
 
 
 export interace IShopContext {
@@ -16,6 +22,16 @@ export interace IShopContext {
 export const ShopContext = createContext<IShopContext>(defaultVal);
 
 export const ShopContextProvider = (props) => {
+
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const [cartItems, setCartItems] = useState<{ string: number } | {}>({});
+    const [availableMoney, setAvailableMoney] = useState<number>(0);
+    const [purchasedItems, setPurchasedItems] = useState<IProduct[]>([]);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(cookies.access_token !== null);
+
+    const { products } = useGetProducts();
+    const { headers } = useGetToken();
+    const navigate = useNavigate();
 
     const fetchAvailableMoney = () => {
         try {
