c3f684e376bf72b01d44f14556ae7648a213e321
Sam Reither <samsreither@gmail.com>
2025-02-20 20:37:33 -0500

    edits

--- frontend/src/pages/purchased-items/index.tsx
+++ frontend/src/pages/purchased-items/index.tsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { ShopContext, IShopContext } from "../../context/shop-context";
 
 export const PurchasedItemsPage = () => {
+    const { purchasedItems, addToCart, getCartItemCount } = useContext<IShopContext>(ShopContext);
+
     return (
-        <div className="purchased-items-page"></div>
+        <div className="purchased-items-page">
+            <h1> Previously Purchased Items </h1>
+            <div className="purchased-items">
+
+            </div>
+        </div>
     )
 }
